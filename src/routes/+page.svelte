da3de11d9d84849b77a7837f233ebf41e73a00a8
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2023-08-23 19:47:32 +0100

    yeet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,58 +3,57 @@
 </script>
 
 <script type="ts">
-    import src from "./+page.svelte?raw";
-    import rects from "$lib/mainpage/rects.svg";
     import { onDestroy, onMount } from "svelte";
 
-    let scroll: number = 0;
-    let els: {[item: string]: HTMLDivElement} = {};
+    let svg: SVGElement | null = null;
+    let clientHeight: number, clientWidth: number;
+    let positioner: SVGGElement | null = null;
+    let weight:number=5;
 
-    let mouseX: number, mouseY: number = 0;
+    let i: ReturnType<typeof setTimeout> | null = null;
 
-    function transform(who: HTMLDivElement, xoff: number, yoff: number, parallax: number) {
-        if (who !== undefined)
-        who.style.transform = `translate(${xoff * parallax}px, ${yoff * parallax}px)`
+        
+    let constructed: string[] = [];
+    let paths: {x:number,y:number}[] = [];
+    let offset: {x:number, y:number}[] = [];
+
+    for(let i = 0; i <= 50; i++) {
+        let progress = i / 50;
+        let angle = progress * Math.PI / 2 + Math.PI / 2;
+        paths.push({
+            x: Math.sin(angle),
+            y: Math.cos(angle)
+        })
+        offset.push({
+            x: 0,
+            y: 0
+        })
+
+        constructed.push("");
     }
 
     $: {
-        if (!import.meta.env.SSR) {
-            let xoff = mouseX / (window.innerWidth / 2) - 1;
-            let yoff = mouseY / (window.innerHeight / 2) - 1;
-
-            if (matchMedia("(prefers-reduced-motion: reduce)").matches) {
-                xoff = 0;
-                yoff = 0;
-            }
-            
-            transform(els.hero, xoff, yoff, 40);
-            transform(els.front, xoff, yoff, 100);
-            transform(els.pattern, xoff, yoff, 90);
-            transform(els.box1, xoff, yoff, 80);
-            transform(els.box2, xoff, yoff, 60);
-            transform(els.box3, xoff, yoff, 30)
-            transform(els.box4, xoff, yoff, 50)
+        for (let i = 0; i < offset.length; i++) {
+            let command = i === 0 ? "M" : "L";
+            let node = paths[i];
+            let off = offset[i];
+            constructed[i] = command + " " + (node.x + off.x) + " " + (node.y + off.y);
         }
     }
 
-    function handleMouseMove(ev: MouseEvent) {
-        mouseX = ev.clientX;
-        mouseY = ev.clientY;
+    $: {
+        if (!import.meta.env.SSR && positioner !== null)
+        positioner.setAttributeNS(null, "transform", `translate(${0}, ${clientHeight}) scale(${clientWidth / 1.5}, ${clientWidth / 1.5})`);
+        weight = 1 / clientWidth * 200;
     }
-
-    let i: ReturnType<typeof setTimeout> | null = null;
-    let current = "";
 
     onMount(() => {
         i = setInterval(() => {
-            current += src.at(current.length);
-
-            if (current.length >= src.length) {
-                current = "";
-            }
-
-            if (matchMedia("(prefers-reduced-motion: reduce)").matches) {
-                current = src;
+            if(Math.random() > 0.9) {
+                let n = offset[Math.floor(Math.random() * offset.length)];
+                n.x = Math.random() * 0.1 - 0.05;
+                n.y = Math.random() * 0.1 - 0.05;
+                offset = offset;
             }
         }, 1)
     })
@@ -64,16 +63,12 @@
 
 <div class="hero-root">
     <div style:position="relative" style:height="100vh">
-        <div aria-hidden="true" class="hero" bind:this={els.hero}></div>
-        <h1 class="hero-wrapper" bind:this={els.front}>Applejuice</h1>
-        <div aria-hidden="true" class="hero-pattern" bind:this={els.pattern}></div>
-
-        <div aria-hidden="true" class="deco-box squared-box"  style:background-repeat="repeat" style:background-image="url({rects})" style:left="50px" style:bottom="50px" style:width="50%" style:height="200px" bind:this={els.box1}/>
-        <div aria-hidden="true" class="deco-box" style:font-family="monospace" style:white-space="pre" style:color="#00ffff" style:font-size="0.2em" style:right="50px" style:top="50px" style:width="50%" style:height="200px" bind:this={els.box2}>{current}</div>
-
-        <div aria-hidden="true" class="deco-box" style:left="50px" style:top="50px" style:width="100px" style:height="50px" bind:this={els.box3}/>
-        
-        <div aria-hidden="true" class="deco-box" style:right="50px" style:bottom="50px" style:width="100px" style:height="50px" bind:this={els.box4}/>
+        <svg width="100%" height="100%" bind:this={svg} style:mix-blend-mode="difference">
+            <g bind:this={positioner}>
+                <path fill="transparent" stroke="white" stroke-width={weight} d={constructed.join(" ")}></path>
+            </g>
+        </svg>
+        <h1 class="hero-wrapper">Applejuice</h1>
     </div>
 </div>
 
@@ -108,14 +103,18 @@
     }
 
     .hero-root {
-        height: 110vh;
+        
+        background-image: url("$lib/mainpage/background.svg");
+        background-repeat: repeat;
+        background-position: center center;
+        background-size: calc(100px);
+
+        height: 100vh;
 
         overflow: hidden;
         position: relative;
         font-size: min(20vh, 5vw);
         filter: grayscale(100%) brightness(0.7);
-
-        clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 93%, 60% 95%, 50% 93%, 30% 100%, 20% 93%, 0 100%);
 
         user-select: none;
     }
@@ -135,15 +134,6 @@
         margin: 2px;
     }
 
-    .hero {
-        transform: translateZ(400px);
-        --bar-rotation: 40deg;
-        background-image: linear-gradient(var(--bar-rotation), #ff0000 50%, #ffffff 50.01%);
-
-        position: absolute;
-        inset: -100px;
-    }
-
     .hero-root::after {
         content: "";
         height: 30vh;
@@ -157,12 +147,6 @@
 
         background-image: linear-gradient(#ffffff00 0%, #00ffff 50%, #00ffff 100%);
         animation: prompter 3s infinite;
-    }
-
-    @media (prefers-reduced-motion: no-preference) {
-        .squared-box {
-            animation: switch 1s infinite;
-        }
     }
 
     @keyframes switch {
@@ -218,24 +202,6 @@
         position: absolute;
         inset: -100px;
     }
-    
-    .hero-pattern {
-        background-image: url("$lib/mainpage/background.svg");
-        background-repeat: repeat;
-        background-position: center center;
-        background-size: calc(100px);
-
-        mix-blend-mode: difference;
-
-        position: absolute;
-        inset: -100px;
-    }
-
-    .deco-box {
-        position: absolute;
-        mix-blend-mode: difference;
-        outline: 2px #00ffff solid;
-    }
 
     :global(body) {
         margin: 0;
@@ -250,5 +216,4 @@
     }
 </style>
 
-
-<svelte:window bind:scrollY={scroll} on:mousemove={handleMouseMove}/>
+<svelte:window bind:innerHeight={clientHeight} bind:innerWidth={clientWidth}/>
